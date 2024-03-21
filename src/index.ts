3afefad68efa7c1bedbd6fd63f09af4c8b98e25a
Paul Kompfner <kompfner@gmail.com>
2024-03-21 10:26:30 -0400

    Minor refactor for readability

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,57 @@
-import Daily, { DailyMeetingState } from "@daily-co/daily-js";
+import Daily, { DailyCall, DailyMeetingState } from "@daily-co/daily-js";
+
+function setupCallClient() {
+  const call = Daily.createCallObject({
+    url: "https://paulk.staging.daily.co/hello",
+  });
+  //@ts-ignore
+  window.call = call;
+  return call;
+}
+
+function setupPublishedCamViewListeners(call: DailyCall) {
+  call.on("participant-updated", (event) => {
+    if (!event.participant.local) {
+      return;
+    }
+    updatePublishedCamView(event.participant.tracks.video.track);
+  });
+  call.on("left-meeting", () => updatePublishedCamView(null));
+  call.on("error", () => updatePublishedCamView(null));
+}
+
+function setupJoinLeaveButtonStateListeners(call: DailyCall) {
+  call.on("joining-meeting", () => updateJoinLeaveButton(call.meetingState()));
+  call.on("joined-meeting", () => updateJoinLeaveButton(call.meetingState()));
+  call.on("left-meeting", () => updateJoinLeaveButton(call.meetingState()));
+  call.on("error", () => updateJoinLeaveButton(call.meetingState()));
+  call.on("loading", () => updateJoinLeaveButton(call.meetingState()));
+  call.on("loaded", () => updateJoinLeaveButton(call.meetingState()));
+}
+
+function setupJoinLeaveButtonClickHandler(call: DailyCall) {
+  const joinLeaveButton = document.getElementById(
+    "join-or-leave-call"
+  ) as HTMLButtonElement;
+  joinLeaveButton.addEventListener("click", () => {
+    const meetingState = call.meetingState();
+    updateJoinLeaveButton(meetingState, true);
+    switch (meetingState) {
+      case "new":
+      case "left-meeting":
+      case "error":
+        call.join();
+        break;
+      case "joined-meeting":
+        call.leave();
+        break;
+      default:
+        // Button should've been disabled in all other meeting states, so it
+        // shouldn't be possible to get here
+        break;
+    }
+  });
+}
 
 function updatePublishedCamView(track?: MediaStreamTrack | null) {
   const publishedCamElement = document.getElementById(
@@ -42,53 +95,13 @@ function updateJoinLeaveButton(
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const call = Daily.createCallObject({
-    url: "https://paulk.staging.daily.co/hello",
-  });
-  //@ts-ignore
-  window.call = call;
+  const call = setupCallClient();
 
-  // Setup event listener for published cam view
-  call.on("participant-updated", (event) => {
-    if (!event.participant.local) {
-      return;
-    }
-    updatePublishedCamView(event.participant.tracks.video.track);
-  });
-  call.on("left-meeting", () => updatePublishedCamView(null));
-  call.on("error", () => updatePublishedCamView(null));
-
-  // Setup event listeners for button state
-  call.on("joining-meeting", () => updateJoinLeaveButton(call.meetingState()));
-  call.on("joined-meeting", () => updateJoinLeaveButton(call.meetingState()));
-  call.on("left-meeting", () => updateJoinLeaveButton(call.meetingState()));
-  call.on("error", () => updateJoinLeaveButton(call.meetingState()));
-  call.on("loading", () => updateJoinLeaveButton(call.meetingState()));
-  call.on("loaded", () => updateJoinLeaveButton(call.meetingState()));
-
-  // Initialize join/leave button
+  // Initialize UI
   updateJoinLeaveButton(call.meetingState());
 
-  // Setup event listener for join/leave button click
-  const joinLeaveButton = document.getElementById(
-    "join-or-leave-call"
-  ) as HTMLButtonElement;
-  joinLeaveButton.addEventListener("click", () => {
-    const meetingState = call.meetingState();
-    updateJoinLeaveButton(meetingState, true);
-    switch (meetingState) {
-      case "new":
-      case "left-meeting":
-      case "error":
-        call.join();
-        break;
-      case "joined-meeting":
-        call.leave();
-        break;
-      default:
-        // Button should've been disabled in all other meeting states, so it
-        // shouldn't be possible to get here
-        break;
-    }
-  });
+  // Wire up listeners/handlers
+  setupPublishedCamViewListeners(call);
+  setupJoinLeaveButtonStateListeners(call);
+  setupJoinLeaveButtonClickHandler(call);
 });
